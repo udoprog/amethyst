d612096f2bb7c898005c3b9f546b15821b3d811b
John-John Tedro <udoprog@tedro.se>
2018-11-30 11:40:24 +0100

    Remove dynamic namespace and replace old code

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,22 +1,16 @@
 //! Contains common types that can be glob-imported (`*`) for convenience.
 
 pub use crate::{
+    app::{Application, ApplicationBuilder, CoreApplication, CoreApplicationBuilder},
     callback_queue::{Callback, CallbackQueue},
     config::Config,
     core::{SystemExt, WithNamed},
     ecs::prelude::{Builder, World},
     game_data::{DataInit, GameData, GameDataBuilder},
-    state_event::StateEvent,
-};
-
-#[cfg(not(feature = "dynamic_app"))]
-pub use crate::{
-    app::{Application, ApplicationBuilder, CoreApplication},
     state::{
-        EmptyState, EmptyTrans, SimpleState, SimpleTrans, State, StateData, StateMachine, States,
-        Trans, TransEvent,
+        GlobalCallback, MapStateStorage, SingletonStateStorage, State, StateCallback, StateError,
+        StateStorage, States,
     },
+    state_event::StateEvent,
+    trans::{Trans, TransEvent},
 };
-
-#[cfg(feature = "dynamic_app")]
-pub use crate::dynamic::*;
